e0f22eeb06c9abd1228024111c7adc53d66913a0
Jonas Bögle <Jonas@sandstorm-projects.de>
2019-02-26 02:56:59 +0100

    minor changes

--- join_leave.js
+++ join_leave.js
@@ -5,43 +5,44 @@
 
 registerPlugin({
     name: 'Join/Leave',
-    version: '1.0.0',
+    version: '1.0.1',
     description: 'Let the bot join or leave your channel via commands.',
     author: 'Jonas Bögle <jonas@boegle.me>',
-    engine: ">= 1.0.0",
-    backends: ["discord"],
+    engine: '>= 1.0.0',
+    backends: ['discord'],
     vars: []
 }, (_, config, meta) => {
-    
     const event = require('event')
     const engine = require('engine')
     const backend = require('backend')
 
     engine.log(`Loaded ${meta.name} v${meta.version} by ${meta.author}.`)
 
-    event.on("load", () => {
-        const command = require("command")
+    event.on('load', () => {
+        // @ts-ignore
+        const command = require('command')
         if (!command)
-            throw new Error("Command.js library not found! Please download Command.js and enable it to be able use this script!")
+            throw new Error('Command.js library not found! Please download Command.js and enable it to be able use this script!')
 
-        command.createCommand("join")
-        .help("Moves the SinusBot to your channel")
-        .manual("This will move the SinusBot into your channel")
+        command.createCommand('join')
+        .help('Moves the SinusBot to your channel')
+        .manual('This will move the SinusBot into your channel')
         .checkPermission(hasPermission)
         .exec((client, args, reply) => {
           var channel = client.getChannels()[0]
           if (!channel) {
-              return reply("Unable to join your channel")
+              return reply('Unable to join your channel')
           }
           backend.getBotClient().moveTo(channel)
         })
 
-        command.createCommand("leave")
-        .help("Disconnects the SinusBot")
-        .manual("This will disconnect the SinusBot from the voice channel")
+        command.createCommand('leave')
+        .help('Disconnects the SinusBot')
+        .manual('This will disconnect the SinusBot from the voice channel')
         .checkPermission(hasPermission)
-        .exec((client, args, reply) => {
-            backend.getBotClient().moveTo("")
+        .exec(() => {
+            // @ts-ignore
+            backend.getBotClient().moveTo('')
         })
     })
 
@@ -53,10 +54,10 @@ registerPlugin({
      */
     function hasPermission(client) {
         // try to find a sinusbot user that matches
-        let matches = engine.getUsers().filter(user => 
+        let matches = engine.getUsers().filter(user =>
             // does the UID match?
             user.tsUid() == client.uid() ||
-            // does a group ID match?
+            // or does a group ID match?
             client.getServerGroups().map(group => group.id()).includes(user.tsGroupId())
         )
 
