32da5c26708a103d1cd0c0b0b4715f78c6af32b7
Jonas Bögle <Jonas@sandstorm-projects.de>
2019-04-07 01:59:49 +0200

    update Uptimerobot to sinusbot 1.0.0

--- uptimerobot.js
+++ uptimerobot.js
@@ -5,15 +5,18 @@
 
 registerPlugin({
     name: 'Uptimerobot - Server Status/Uptime Monitoring',
-    version: '2.1.2',
+    version: '3.0.0',
     description: 'Informs you about the status of a server configured on uptimerobot.com',
     author: 'Jonas Bögle (irgendwr)',
+    engine: '>= 1.0.0',
+    backends: ['ts3'],
+    requiredModules: ['http'],
     vars: [
         {
             name: 'info',
-            title: 'Placeholders: \n%name%, %uptime%, %url%, %port%, %type%, %status%, %id%, %created%, %avg_response_time%, %last_response_time%, %ssl.brand%, %ssl.product%, %ssl.expires%',
-            description: 'bar'
-        }, {
+            title: 'Placeholders: \n%name%, %uptime%, %url%, %port%, %type%, %status%, %id%, %created%, %avg_response_time%, %last_response_time%, %ssl.brand%, %ssl.product%, %ssl.expires%'
+        },
+        {
             name: 'servers',
             title: 'Servers',
             type: 'array',
@@ -22,128 +25,154 @@ registerPlugin({
                     name: 'channelEnabled',
                     title: 'Show status in channel',
                     type: 'checkbox'
-                }, {
+                },
+                {
                     name: 'channel',
                     title: 'Channel',
                     type: 'channel',
                     indent: 3,
                     conditions: [{ field: 'channelEnabled', value: true }]
-                }, {
+                },
+                {
                     name: 'channelName',
                     title: 'Channel Name',
                     type: 'string',
                     placeholder: '[cspacer]%name% is %status%',
+                    default: '[cspacer]%name% is %status%',
                     indent: 3,
                     conditions: [{ field: 'channelEnabled', value: true }]
-                }, {
+                },
+                {
                     name: 'channelDescription',
                     title: 'Channel Description',
                     type: 'multiline',
                     placeholder: '[SIZE=12][B]%name%[/B][/SIZE]\n[B]Status[/B]: %status%\n[B]Uptime[/B]: %uptime%',
+                    default: '[SIZE=12][B]%name%[/B][/SIZE]\n[B]Status[/B]: %status%\n[B]Uptime[/B]: %uptime%',
                     indent: 3,
                     conditions: [{ field: 'channelEnabled',  value: true }]
-                }, {
+                },
+                {
                     name: 'chatEnabled',
                     title: 'Send a server-message if a servers status changes',
                     type: 'checkbox'
-                }, {
+                },
+                {
                     name: 'chatMessage',
                     title: 'Message',
                     type: 'string',
                     placeholder: '[B]%name%[/B] is [B]%status%[/B]',
+                    default: '[B]%name%[/B] is [B]%status%[/B]',
                     indent: 3,
                     conditions: [{ field: 'chatEnabled', value: true }]
-                }, {
+                },
+                {
                     name: 'apikey',
                     title: 'Monitor-specific API key',
                     type: 'string'
                 }
             ]
-        }, {
+        },
+        {
             name: 'interval',
             title: 'Refresh interval (in seconds)',
             type: 'number',
-            placeholder: '60'
+            placeholder: '60',
+            default: 60
+        },
+        {
+            name: 'customText',
+            title: 'Show/hide custom text options',
+            type: 'checkbox'
+        },
+        {
+            name: 'textUp',
+            title: '"up"',
+            type: 'string',
+            placeholder: 'up',
+            default: 'up',
+            conditions: [{ field: 'customText', value: true }]
+        },
+        {
+            name: 'textDown',
+            title: '"down"',
+            type: 'string',
+            placeholder: 'down',
+            default: 'down',
+            conditions: [{ field: 'customText', value: true }]
+        },
+        {
+            name: 'textPaused',
+            title: '"paused"',
+            type: 'string',
+            placeholder: 'paused',
+            default: 'paused',
+            conditions: [{ field: 'customText', value: true }]
+        },
+        {
+            name: 'textUnknown',
+            title: '"unknown"',
+            type: 'string',
+            placeholder: 'unknown',
+            default: 'unknown',
+            conditions: [{ field: 'customText', value: true }]
         }
     ]
-}, function (sinusbot, config, info) {
+}, (_, config, meta) => {
+    const engine = require('engine')
+    const backend = require('backend')
+    const format = require('format')
+    const http = require('http')
 
-    // include modules
-    var event = require('event')
-    var engine = require('engine')
-    var backend = require('backend')
-    var format = require('format')
-
-    // set default config values
-    config.interval = config.interval || 60
-    engine.saveConfig(config)
-
-    var servers = config.servers
-    var log = new Logger()
-    log.debug = false
-
-    // check config
+    let servers = config.servers
     if (!servers || servers.length == 0) {
-        log.e('No servers configured.')
+        engine.log('No servers configured.')
         return
     }
 
-    // initialize
-    servers.forEach(function (server, i) {
-        if (server.channelEnabled) {
-            servers[i].channel = backend.getChannelByID(server.channel)
+    engine.log(`Loaded ${meta.name} v${meta.version} by ${meta.author}.`)
 
-            if (!servers[i].channel) {
-                log.w('Invalid channel in ' + (i + 1) + '. server, ignoring...')
-                servers[i].channelEnabled = false
-            }
-            servers[i].channelName = server.channelName || '[cspacer]%name% is %status%'
-            servers[i].channelDescription = server.channelDescription || '[SIZE=12][B]%name%[/B][/SIZE]\n[B]Status[/B]: %status%\n[B]Uptime[/B]: %uptime%'
-        }
-        servers[i].chatMessage = server.chatMessage || '[B]%name%[/B] is [B]%status%[/B]'
-    })
+    // uncomment the following lines if you want to enable the status command:
 
-    // log info on startup
-    log.d(servers.length + ' servers configured')
-    log.i('debug messages are ' + (log.debug ? 'en' : 'dis') + 'abled')
-    log.i(info.name + ' v' + info.version + ' by ' + info.author + ' loaded successfully.')
-
-    event.on('chat', function (ev) {
-        if (ev.text == '.uptimerobot') {
-            servers.forEach(function (server) {
-                fetchData(server, function (data) {
-                    ev.client.chat(
-                        parse(server.chatMessage, data, true, 1024)
-                    )
-                })
-            })
-        }
-    })
+    // const event = require('event')
+    // event.on('load', () => {
+    //     // @ts-ignore
+    //     const command = require("command")
+    //     if (!command) return engine.log("Command.js not found! Please be sure to install and enable Command.js")
+    //     // @ts-ignore
+    //     const {createCommand, createArgument} = command
+        
+    //     createCommand('status')
+    //     .help('Shows uptimerobot status')
+    //     .manual('Shows uptimerobot status')
+    //     //.checkPermission((/** @type {Client} */client) => client.uid() === '')
+    //     .exec((client, args, reply, /** @implements {Message} */ev) => {
+    //         servers.forEach(function (server) {
+    //             fetchData(server, function (data) {
+    //                 reply(parse(server.chatMessage, data, true, 1024))
+    //             })
+    //         })
+    //     })
+    // })
 
     refresh()
-    setInterval(refresh, config.interval * 1000)
+    setInterval(refresh, (config.interval || 60) * 1000)
 
     function refresh() {
-        servers.forEach(function (server, i) {
-            fetchData(server, function (data) {
+        for (let server of servers) {
+            fetchData(server, data => {
+                const channel = backend.getChannelByID(server.channel);
+
                 if (server.channelEnabled) {
-                    server.channel.setName(
-                        parse(server.channelName, data, false, 40)
-                    )
-                    server.channel.setDescription(
-                        parse(server.channelDescription, data, true, 8192)
-                    )
+                    channel.setName(parse(server.channelName, data, false, 40))
+                    channel.setDescription(parse(server.channelDescription, data, true, 8192))
                 }
-
                 if (server.chatEnabled && server.lastStatus != data.status) {
-                    backend.chat(
-                        parse(server.chatMessage, data, true, 1024)
-                    )
+                    backend.chat(parse(server.chatMessage, data, true, 1024))
                 }
 
-                servers[i].lastStatus = data.status
+                server.lastStatus = data.status
             })
-        })
+        }
     }
 
     /**
@@ -161,7 +190,7 @@ registerPlugin({
             response_times_average: 1
         })
 
-        sinusbot.http({
+        http.simpleRequest({
             method:  'POST',
             url:     'https://api.uptimerobot.com/v2/getMonitors',
             timeout: 6000,
@@ -169,33 +198,32 @@ registerPlugin({
             headers: {
                 'Content-Type': 'application/json'
             }
-        }, function (error, response) {
+        }, (error, response) => {
             // check whether request was successfull
             if (error || response.statusCode != 200) {
-                log.e('API request failed: ' + (error || response.statusCode))
+                engine.log(`[Error] API request failed: ${(error || 'HTTP '+response.statusCode)}`)
                 return
             }
 
             var data
             try {
-                data = JSON.parse(response.data)
+                data = JSON.parse(response.data.toString())
             } catch (err) {
-                log.e('Unable to parse data: ' + err.message)
-                log.d('Response: ' + response.data)
+                engine.log(`[Error] Unable to parse data: ${err}`)
+                engine.log(`Response: ${response.data}`)
             }
 
             // check whether response is valid
-            if (data == undefined) {
+            if (!data) {
                 return
             } else if (data.stat == 'fail') {
-                log.e('API Request failed: ' + JSON.stringify(data.error))
+                engine.log(`[Error] API Request failed: ${JSON.stringify(data.error)}`)
                 return
             }
 
-            // log.d('Data: ' + response.data)
+            // engine.log(`Data: ${response.data}`)
 
             data = data.monitors[0]
-
             callback(data)
         })
     }
@@ -214,6 +242,18 @@ registerPlugin({
         ), len)
     }
 
+    const types = [
+        '',
+        'HTTP(s)',
+        'Keyword',
+        'Ping',
+        'Port'
+    ]
+    const UP = config.textUp || 'up';
+    const DOWN = config.textDown || 'down';
+    const PAUSED = config.textPaused || 'paused';
+    const UNKNOWN = config.textUnknown || 'unknown';
+
     /**
      * Replaces placeholders
      * @param {string} str String
@@ -222,22 +262,15 @@ registerPlugin({
      * @returns {string}
      */
     function replacePlaceholders(str, data, fmt) {
-        var types = [
-            '',
-            'HTTP(s)',
-            'Keyword',
-            'Ping',
-            'Port'
+        let status = [
+            fmt ? format.color(PAUSED,  '#000000') : PAUSED,
+            fmt ? format.color(UNKNOWN, '#464646') : UNKNOWN,
+            fmt ? format.color(UP,      '#4da74d') : UP,
         ]
-        var status = [
-            fmt ? format.color('paused',  '#000000') : 'paused',
-            fmt ? format.color('unknown', '#464646') : 'unknown',
-            fmt ? format.color('up',      '#4da74d') : 'up',
-        ]
-        status[8] = fmt ? format.color('down', '#ff5e21') : 'down' // seems down
-        status[9] = fmt ? format.color('down', '#ff2121') : 'down'
+        status[8] = fmt ? format.color(DOWN, '#ff5e21') : DOWN // "seems down"
+        status[9] = fmt ? format.color(DOWN, '#ff2121') : DOWN
 
-        str = str.replace(/%name%/gi, data.friendly_name)
+        return str.replace(/%name%/gi, data.friendly_name)
         .replace(/%uptime%/gi, data.all_time_uptime_ratio + '%')
         .replace(/%(url|ip)%/gi, data.url)
         .replace(/%port%/gi, data.port)
@@ -250,41 +283,15 @@ registerPlugin({
         .replace(/%created%/gi, new Date(data.create_datetime * 1000).toLocaleString())
         .replace(/%last_response_time%/gi, data.response_times && data.response_times.length == 1 ? data.response_times[0].value + 'ms' : '')
         .replace(/%avg_response_time%/gi, data.average_response_time ? data.average_response_time + 'ms' : '')
-
-        return str
     }
 
     /**
-     * Shortens a string to a specified length
+     * Truncates a string to a specified length
      * @param {string} str String
      * @param {number} len Max. length of the string
      * @returns {string}
      */
     function trunc(str, len) {
         return (str.length > len) ? str.substr(0, len - 1) + '…' : str
-    }
-
-    /**
-     * Creates a logging interface
-     * @requires engine
-     */
-    function Logger() {
-        this.debug = false
-        this.log = function (level, msg) {
-            engine.log('[' + level + '] ' + msg)
-        }
-        this.e = function (msg) {
-            this.log('ERROR', msg)
-        }
-        this.w = function (msg) {
-            this.log('WARN', msg)
-        }
-        this.i = function (msg) {
-            this.log('INFO', msg)
-        }
-        this.d = function (msg) {
-            if (this.debug)
-                this.log('DEBUG', msg)
-        }
     }
 })
