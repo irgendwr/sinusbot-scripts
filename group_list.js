d5b87722b3c7b625688a0576caa1308c0c81d4fe
Jonas Bögle <jonas@boegle.me>
2019-06-06 12:31:09 +0200

    update grouplist

--- group_list.js
+++ group_list.js
@@ -5,72 +5,38 @@
 
 registerPlugin({
     name: 'Group List',
-    version: '1.0.0',
+    version: '1.1.0',
     description: 'List the servers groups and their IDs with the `!groups` command.',
     author: 'Jonas Bögle (irgendwr)',
     backends: ['ts3', 'discord'],
     vars: []
-}, function (sinusbot, config, info) {
+}, (_, config, meta) => {
+    const event = require('event')
+    const engine = require('engine')
+    const backend = require('backend')
+    const format = require('format')
 
-    // include modules
-    var event = require('event')
-    var engine = require('engine')
-    var backend = require('backend')
-    var format = require('format')
+    engine.log(`Loaded ${meta.name} v${meta.version} by ${meta.author}.`)
 
-    var log = new Logger()
-    log.debug = false
+    event.on('load', () => {
+        const command = require('command')
+        if (!command)
+            throw new Error('Command.js library not found! Please download Command.js and enable it to be able use this script!')
 
-    // log info on startup
-    log.i('debug messages are ' + (log.debug ? 'en' : 'dis') + 'abled')
-    log.i(info.name + ' v' + info.version + ' by ' + info.author + ' loaded successfully.')
-    log.d('backend: ' + engine.getBackend())
-
-    event.on('chat', function (ev) {
-        if (ev.text === '!groups') {
-            // FIXME: workaround for discord
-            if (ev.mode == 2 && ev.channel == undefined) {
-                ev.mode = 1
-                log.d('[workaround] detected message type: ' + ev.mode)
-            }
-
+        command.createCommand('groups')
+        .help('Lists the servers groups and their IDs')
+        .manual('Lists the servers groups and their IDs')
+        .exec((/** @type {Client} */client, /** @type {object} */args, /** @type {(msg:string)=>void} */reply) => {
             var resp = format.bold('Groups:')
+            // TODO: split into multiple messages if too long
             backend.getServerGroups().forEach(function (group) {
                 resp += '\n * `' + group.name() + '`, ID: `' + group.id() + '`'
             })
 
-            switch (ev.mode) {
-                case 1: ev.client.chat(resp); break
-                case 2: ev.channel.chat(resp); break
-                case 3: backend.chat(resp); break
-            }
-        }
-    })
+            reply(resp)
+        })
 
-    /**
-     * Creates a logging interface
-     * @requires engine
-     */
-    function Logger() {
-        this.debug = false
-        this.log = function (level, msg) {
-            if (typeof msg == 'object') {
-                msg = JSON.stringify(msg)
-            }
-            engine.log('[' + level + '] ' + msg)
-        }
-        this.e = function (msg) {
-            this.log('ERROR', msg)
-        }
-        this.w = function (msg) {
-            this.log('WARN', msg)
-        }
-        this.i = function (msg) {
-            this.log('INFO', msg)
-        }
-        this.d = function (msg) {
-            if (this.debug)
-                this.log('DEBUG', msg)
-        }
-    }
+        //TODO: maybe add a command to search for groups?
+        //TODO: add permissions to commands? via settings or sinusbot permissions?
+    })
 })
