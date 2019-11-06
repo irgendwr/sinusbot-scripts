11459e5c25ffbd131b0607d4ccbbb2ce64b6ec23
Jonas Bögle <jonas@boegle.me>
2019-11-06 23:32:57 +0100

    [WIP] restructure custom commands

--- custom_commands.js
+++ custom_commands.js
@@ -4,166 +4,310 @@
  */
 
 registerPlugin({
-    name: 'Simple custom commands',
+    name: 'Custom commands - NOT FUNCTIONAL, WORK IN PROGRESS', //TODO: remove when finished
     version: '1.3.0',
     description: 'Create your own custom commands.',
     author: 'Jonas Bögle (irgendwr)',
     backends: ['ts3', 'discord'],
     vars: [
         {
-            name: 'cmdPrefix',
-            title: 'Command prefix:',
-            type: 'select',
-            placeholder: '!',
-            options: [
-                '!',
-                '.',
-                ' (none)'
-            ]
-        },
-        {
             name: 'commands',
-            title: 'Commands:',
+            title: 'Commands',
             type: 'array',
             vars: [
                 {
-                    name: 'command',
-                    title: 'Command:',
-                    type: 'string'
+                    name: 'name',
+                    title: 'Command Name',
+                    placeholder: 'example: "info"',
+                    type: 'string',
                 },
                 {
-                    name: 'response',
-                    title: 'Response: (placeholders: %username%, %uid%, %dbid%, %mention%, %description%, %ping%, %total_connections%, %packetloss%, %bytes_sent%, %bytes_received%, %ip%, %first_join%, %os%, %version%, %clients_count%, %clients%, %channels_count%)',
-                    type: 'multiline'
-                }
+                    name: 'description',
+                    title: 'Description',
+                    placeholder: 'example: "Responds with the users uid."',
+                    type: 'string',
+                },
+                {
+                    name: 'prefix_change',
+                    title: 'Change Command Prefix (default is in Instance Settings)',
+                    type: 'checkbox',
+                    default: false,
+                },
+                {
+                    name: 'prefix',
+                    title: 'Command Prefix',
+                    placeholder: 'example: "!"',
+                    type: 'string',
+                    indent: 2,
+                    conditions: [{ field: 'prefix_change', value: true }],
+                },
+                {
+                    name: 'actions',
+                    title: 'Actions',
+                    type: 'array',
+                    vars: [
+                        {
+                            name: 'action_type',
+                            title: 'What should happen?',
+                            type: 'select',
+                            options: [
+                                'Respond with a message',
+                                'Play a Track',
+                            ],
+                            default: '0',
+                        },
+                        {
+                            name: 'response_type',
+                            title: 'Where should the bot respond?',
+                            type: 'select',
+                            options: [
+                                'In the same chat as the user wrote in',
+                                'Always answer in private chat'
+                            ],
+                            default: '0',
+                            conditions: [{ field: 'action_type', value: '0' }],
+                        },
+                        {
+                            name: 'response_text',
+                            title: 'Response',
+                            type: 'multiline',
+                            placeholder: 'example: "Hi {mention}, your uid is: {uid}',
+                            conditions: [{ field: 'action_type', value: '0' }],
+                        },
+                        {
+                            title: 'Placeholders (some are TS3 only):\n username, mention, uid, dbid, description, ping, total_connections, packetloss, bytes_sent, bytes_received, ip, first_join, os, version, clients_count, clients, channels_count',
+                            conditions: [{ field: 'action_type', value: '0' }],
+                        },
+                        {
+                            name: 'track',
+                            title: 'Track',
+                            type: 'track',
+                            conditions: [{ field: 'action_type', value: '1' }],
+                        },
+                    ]
+                },
+                {
+                    name: 'restrict',
+                    title: 'Restrict usage?',
+                    type: 'checkbox',
+                    default: false,
+                },
+                {
+                    name: 'disable_serverchat',
+                    title: 'Disable in serverchat',
+                    type: 'checkbox',
+                    indent: 2,
+                    default: false,
+                    conditions: [{ field: 'restrict', value: true }],
+                },
+                {
+                    name: 'disable_channelchat',
+                    title: 'Disable in channel chat',
+                    type: 'checkbox',
+                    indent: 2,
+                    default: false,
+                    conditions: [{ field: 'restrict', value: true }],
+                },
+                {
+                    name: 'disable_privatechat',
+                    title: 'Disable in private chat',
+                    type: 'checkbox',
+                    indent: 2,
+                    default: false,
+                    conditions: [{ field: 'restrict', value: true }],
+                },
+                { //TODO: change this
+                    name: 'servergroups_type',
+                    title: 'Servergroups: Blacklist or whitelist?',
+                    type: 'select',
+                    options: [
+                        'Blacklist',
+                        'Whitelist'
+                    ],
+                    default: '0',
+                    indent: 2,
+                    conditions: [{ field: 'restrict', value: true }],
+                },
+                {
+                    name: 'servergroups_list',
+                    title: 'Servergroup IDs (Black-/Whitelist)',
+                    type: 'strings',
+                    indent: 2,
+                    conditions: [{ field: 'restrict', value: true }],
+                },
+                {
+                    name: 'cooldown',
+                    title: 'Cooldown: Only allow command every X seconds (enter 0 to disable)',
+                    placeholder: '0',
+                    type: 'number',
+                    default: 0,
+                    indent: 2,
+                    conditions: [{ field: 'restrict', value: true }],
+                },
             ]
-        },
-        {
-            name: 'responseType',
-            title: 'Where should the bot respond?',
-            type: 'select',
-            placeholder: 'In the same chat as the user wrote in',
-            options: [
-                'In the same chat as the user wrote in',
-                'Always answer in private chat'
-            ]
-        },
-        {
-            name: 'serverChatEnabled',
-            title: 'Listen in the serverchat',
-            type: 'checkbox'
-        },
-        {
-            name: 'channelChatEnabled',
-            title: 'Listen in the channel chat',
-            type: 'checkbox'
-        },
-        {
-            name: 'privateChatEnabled',
-            title: 'Listen in the private chat',
-            type: 'checkbox'
-        },
-        {
-            name: 'cooldown',
-            title: 'Cooldown: only allow command every X seconds (enter 0 to disable)',
-            placeholder: '0',
-            type: 'number'
-        },
-        {
-            name: 'blackOrWhite',
-            title: 'Blacklist or whitelist?',
-            type: 'select',
-            placeholder: 'Blacklist',
-            options: [
-                'Blacklist',
-                'Whitelist'
-            ]
-        },
-        {
-            name: 'sgList',
-            title: 'Black-/Whitelist users with these servergroups:',
-            type: 'array',
-            vars: [{
-                name: 'servergroup',
-                title: 'Servergroup ID',
-                type: 'number'
-            }]
         }
     ]
-}, function (_, config, info) {
+}, function (_, config, meta) {
+    const event = require('event');
+    const engine = require('engine');
+    const backend = require('backend');
+    const media = require('media');
 
-    // include modules
-    var event = require('event')
-    var engine = require('engine')
-    var backend = require('backend')
+    engine.log(`Loaded ${meta.name} v${meta.version} by ${meta.author}.`);
 
-    // set default config values
-    config.cmdPrefix = config.cmdPrefix || 0
-    config.serverChatEnabled = config.serverChatEnabled || false
-    config.channelChatEnabled = config.channelChatEnabled || false
-    config.privateChatEnabled = config.privateChatEnabled || false
-    config.responseType = config.responseType || 0
-    config.cooldown = config.cooldown || 0
-    config.blackOrWhite = config.blackOrWhite || 0
-    engine.saveConfig(config)
-    
-    var prefix = ['!', '.', ''][config.cmdPrefix]
-    var lastUsage = {}
-    var log = new Logger()
-    log.debug = false
+    //TODO: move old placeholders here
+    const placeholders = {
+        foo: () => "bar",
+    };
 
-    // log info on startup
-    log.i('debug messages are ' + (log.debug ? 'en' : 'dis') + 'abled')
-    log.i(info.name + ' v' + info.version + ' by ' + info.author + ' loaded successfully.')
-    log.d('backend: ' + engine.getBackend())
+    /**
+     * @typedef Context
+     * @type {object}
+     * @property {Client} client
+     * @property {object} args
+     * @property {(msg: string) => void} reply
+     * @property {Message} ev
+     * @property {DiscordMessage} message
+     */
 
-    event.on('chat', function (ev) {
-        log.d(ev)
-
-        if (isBlacklisted(ev.client)) {
-            log.d('ignoring command from ' + ev.client.name() + ' due to blacklist')
-            return
+    event.on('load', () => {
+        const command = require('command');
+        if (!command) {
+            engine.log('command.js library not found! Please download command.js and enable it to be able use this script!');
+            return;
         }
 
-        // FIXME: workaround for discord
-        if (ev.mode == 2 && ev.channel == undefined) {
-            ev.mode = 1
-            log.d('[workaround] detected message type: ' + ev.mode)
+        if (!config.commands || config.commands.length == 0) {
+            engine.log("No commands configured.");
+            return;
         }
-        // TODO: add option to only listen in a specified channel
 
-        if (ev.mode == 1 && config.privateChatEnabled ||
-            ev.mode == 2 && config.channelChatEnabled ||
-            ev.mode == 3 && config.serverChatEnabled) {
+        config.commands.forEach((cfg, i) => {
+            let name = cfg.name;
 
-            config.commands.forEach(function (cmd) {
-                if (ev.text == prefix + cmd.command) {
-                    if (config.cooldown > 0 && lastUsage[ev.client.uid()] != undefined &&
-                        (timestamp() - lastUsage[ev.client.uid()]) < config.cooldown) {
+            if (!name || name.length < 1) {
+                engine.log(`Skipping ${i+1}. command: Name is not set.`);
+            }
 
-                            log.d('ignoring command from ' + ev.client.name() + ' due to cooldown')
-                            ev.client.chat('You\'ve reached the cooldown, please stop spamming. (' + (timestamp() - lastUsage[ev.client.uid()]) + ')')
-                            lastUsage[ev.client.uid()] = timestamp()
-                            return
-                    }
+            let cmd;
+            try {
+                cmd = command.createCommand(name);
+            } catch(ex) {
+                engine.log(`Skipping ${i+1}. command due to error: ${ex}`);
+                return;
+            }
+            
+            let description = cfg.description;
+            if (description) {
+                cmd.help(description);
+                cmd.manual(description);
+            }
 
-                    log.d('Received command "' + ev.text + '" from ' + ev.client.name())
-
-                    var resp = replacePlaceholders(cmd.response, ev.client)
-
-                    if (config.responseType == 1 || ev.mode == 1) {
-                        ev.client.chat(resp)
-                    } else if (ev.mode == 2) {
-                        ev.channel.chat(resp)
-                    } else if (ev.mode == 3) {
-                        backend.chat(resp)
-                    }
-
-                    lastUsage[ev.client.uid()] = timestamp()
+            if (cfg.prefix_change) {
+                let prefix = cfg.prefix;
+                if (!prefix || prefix.length < 1) {
+                    engine.log(`Warning in ${i+1}. command: Prefix not set, using default instead.`);
+                } else {
+                    cmd.forcePrefix(prefix);
                 }
-            })
-        }
-    })
+            }
+
+            if (cfg.restrict) {
+                cmd.checkPermission(client => {
+                    //TODO: check servergroup as configured
+                })
+
+                if (cfg.cooldown) {
+                    //TODO: add cooldown as configured
+                    //cmd.addThrottle(...)
+                }
+            }
+
+            let actions = cfg.actions;
+            if (!actions || actions.length < 1) {
+                engine.log(`Warning in ${i+1}. command: No actions set.`);
+                return;
+            } else {
+                let funcs = [];
+                actions.forEach((action, j) => {
+                    switch (action.action_type) {
+                        case 0:
+                        case '0': // Respond with a message
+                            let text = action.response_test;
+                            if (!text || text.length < 1) {
+                                engine.log(`Warning in ${i+1}. command: No message text set in ${j+1}. action.`);
+                                return;
+                            }
+
+                            let type = action.response_type || '0';
+                            if (type == '1') { // private chat
+                                funcs.push(ctx => {
+                                    ctx.client.chat(format(text, ctx, placeholders));
+                                });
+                            } else { // same chat
+                                funcs.push(ctx => {
+                                    ctx.reply(format(text, ctx, placeholders));
+                                });
+                            }
+                            break;
+                        case 1:
+                        case '1': // Play a Track
+                            let track = action.track;
+                            if (!track) {
+                                engine.log(`Warning in ${i+1}. command: No track set in ${j+1}. action.`);
+                                return;
+                            }
+                            funcs.push(() => {
+                                media.playURL(track);
+                            });
+                            break;
+                        default:
+                            engine.log(`Warning in ${i+1}. command: ${j+1}. action is unknown.`);
+                    }
+                });
+                cmd.exec((client, args, reply, ev) => {
+                    if (cfg.restrict) {
+                        if (ev.mode == 3 && cfg.disable_serverchat) {
+                            engine.log('Ignoring command since serverchat is disabled.');
+                            return;
+                        }
+                        if (ev.mode == 2 && cfg.disable_channelchat) {
+                            engine.log('Ignoring command since channelchat is disabled.');
+                            return;
+                        }
+                        if (ev.mode == 1 && cfg.disable_privatechat) {
+                            engine.log('Ignoring command since privatechat is disabled.');
+                            return;
+                        }
+                    }
+
+                    /** @implements {Context} */
+                    let ctx = {
+                        client: client,
+                        args: args,
+                        reply: reply,
+                        ev: ev,
+                        message: ev.message,
+                    }
+
+                    funcs.forEach(func => {
+                        func(ctx);
+                    });
+                })
+            }
+        });
+    });
+
+    /**
+     * Formats a string with placeholders.
+     * @param {string} str
+     * @param {Context} ctx
+     * @param {object} placeholders
+     */
+    function format(str, ctx, placeholders) {
+        //TODO: implement formatter
+        return str;
+    }
 
     /**
      * Loggs a message, requires engine
@@ -220,40 +364,5 @@ registerPlugin({
                 return servergroup.id() == item.servergroup
             })
         })
-    }
-
-    /**
-     * Returns the current timestamp in seconds
-     * @return {number} timestamp
-     */
-    function timestamp() {
-        return Math.floor(Date.now() / 1000)
-    }
-
-    /**
-     * Creates a logging interface
-     * @requires engine
-     */
-    function Logger() {
-        this.debug = false
-        this.log = function (level, msg) {
-            if (typeof msg == 'object') {
-                msg = JSON.stringify(msg)
-            }
-            engine.log('[' + level + '] ' + msg)
-        }
-        this.e = function (msg) {
-            this.log('ERROR', msg)
-        }
-        this.w = function (msg) {
-            this.log('WARN', msg)
-        }
-        this.i = function (msg) {
-            this.log('INFO', msg)
-        }
-        this.d = function (msg) {
-            if (this.debug)
-                this.log('DEBUG', msg)
-        }
     }
 })
