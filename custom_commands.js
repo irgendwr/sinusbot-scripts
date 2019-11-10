9d5887232aa91c2079738c5841b87eaa89936d61
Jonas Bögle <jonas@boegle.me>
2019-11-11 00:30:02 +0100

    implement placeholders and permission

--- custom_commands.js
+++ custom_commands.js
@@ -1,13 +1,38 @@
 /**
  * Forum:  https://forum.sinusbot.com/resources/simple-custom-commands.226/
  * GitHub: https://github.com/irgendwr/sinusbot-scripts
+ * 
+ * @author Jonas Bögle
+ * @license MIT
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 Jonas Bögle
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * @ignore
  */
-
 registerPlugin({
-    name: 'Custom commands - NOT FUNCTIONAL, WORK IN PROGRESS', //TODO: remove when finished
-    version: '1.3.0',
+    name: 'Custom commands',
+    version: '1.4.0',
     description: 'Create your own custom commands.',
-    author: 'Jonas Bögle (irgendwr)',
+    author: 'Jonas Bögle (@irgendwr)',
     backends: ['ts3', 'discord'],
     vars: [
         {
@@ -75,7 +100,7 @@ registerPlugin({
                             conditions: [{ field: 'action_type', value: '0' }],
                         },
                         {
-                            title: 'Placeholders (some are TS3 only):\n username, mention, uid, dbid, description, ping, total_connections, packetloss, bytes_sent, bytes_received, ip, first_join, os, version, clients_count, clients, channels_count',
+                            title: 'Placeholders (some are TS3 only):\n username, mention, uid, dbid, description, ping, total_connections, packetloss, bytes_sent, bytes_received, ip, first_join, os, version, clients_count, clients, channels_count, channels, client_groups_count, client_groups, server_groups_count, server_groups, playing, random(<min>, <max>), randomString(option one, option two, ...), channel_name, channel_id, channel_url, client_channel_name, client_channel_id, client_channel_url',
                             conditions: [{ field: 'action_type', value: '0' }],
                         },
                         {
@@ -92,7 +117,7 @@ registerPlugin({
                     type: 'checkbox',
                     default: false,
                 },
-                {
+                /*{
                     name: 'disable_serverchat',
                     title: 'Disable in serverchat',
                     type: 'checkbox',
@@ -115,8 +140,8 @@ registerPlugin({
                     indent: 2,
                     default: false,
                     conditions: [{ field: 'restrict', value: true }],
-                },
-                { //TODO: change this
+                },*/
+                {
                     name: 'servergroups_type',
                     title: 'Servergroups: Blacklist or whitelist?',
                     type: 'select',
@@ -149,16 +174,12 @@ registerPlugin({
     ]
 }, function (_, config, meta) {
     const event = require('event');
+    const audio = require('audio');
+    const media = require('media');
     const engine = require('engine');
     const backend = require('backend');
-    const media = require('media');
 
     engine.log(`Loaded ${meta.name} v${meta.version} by ${meta.author}.`);
-
-    //TODO: move old placeholders here
-    const placeholders = {
-        foo: () => "bar",
-    };
 
     /**
      * @typedef Context
@@ -169,6 +190,70 @@ registerPlugin({
      * @property {Message} ev
      * @property {DiscordMessage} message
      */
+
+    const placeholders = {
+        username: ctx => ctx.client.name(),
+        uid: ctx => ctx.client.uid(),
+        dbid: ctx => ctx.client.databaseID(),
+        description: ctx => ctx.client.description(),
+        ping: ctx => ctx.client.getPing(),
+        total_connections: ctx => ctx.client.getTotalConnections(),
+        packetloss: ctx => ctx.client.getPacketLoss(),
+        bytes_sent: ctx => ctx.client.getBytesSent(),
+        bytes_received: ctx => ctx.client.getBytesReceived(),
+        ip: ctx => ctx.client.getIPAddress(),
+        first_join: ctx => ctx.client.getCreationTime() > 0 ? new Date(ctx.client.getCreationTime()).toLocaleDateString() : 'unknown',
+        os: ctx => ctx.client.getPlatform(),
+        version: ctx => ctx.client.getVersion(),
+        mention: ctx => ctx.client.getURL(),
+        clients_count: ctx => backend.getClients().length,
+        clients: ctx => backend.getClients().map(client => client.name()).join(', '),
+        channels_count: ctx => backend.getChannels().length,
+        channels: ctx => backend.getChannels().map(channel => channel.name()).join(', '),
+        client_groups_count: ctx => ctx.client.getServerGroups().length,
+        client_groups: ctx => ctx.client.getServerGroups().map(group => group.name()).join(', '),
+        server_groups_count: ctx => backend.getServerGroups().length,
+        server_groups: ctx => backend.getServerGroups().map(group => group.name()).join(', '),
+        playing: ctx => {
+            let track = media.getCurrentTrack();
+            return track && audio.isPlaying() ? formatTrack(track) : 'none';
+        },
+        random: (ctx, ...args) => {
+            let min = parseInt(args[0], 10);
+            let max = parseInt(args[1], 10);
+            return getRandomIntInclusive(min, max);
+        },
+        randomString: (ctx, ...args) => {
+            if (args.length === 0) return "";
+            return args[Math.floor(Math.random() * args.length)];
+        },
+        channel_name: ctx => {
+            let channel = ctx.channel;
+            return channel ? channel.name() : 'none';
+        },
+        channel_id: ctx => {
+            let channel = ctx.channel;
+            return channel ? channel.id() : 'none';
+        },
+        channel_url: ctx => {
+            let channel = ctx.channel;
+            if (!channel) return 'none';
+            return engine.getBackend() === 'discord' ? `https://www.discordapp.com/channels/${channel.id()}` : channel.getURL();
+        },
+        client_channel_name: ctx => {
+            let channels = ctx.client.getChannels();
+            return channels && channels.lenght !== 0 ? channels[0].name() : 'none';
+        },
+        client_channel_id: ctx => {
+            let channels = ctx.client.getChannels();
+            return channels && channels.lenght !== 0 ? channels[0].id() : 'none';
+        },
+        client_channel_url: ctx => {
+            let channels = ctx.client.getChannels();
+            if (!channels || channels.lenght == 0) return 'none';
+            return engine.getBackend() === 'discord' ? `https://www.discordapp.com/channels/${channels[0].id()}` : channels[0].getURL();
+        },
+    };
 
     event.on('load', () => {
         const command = require('command');
@@ -192,6 +277,7 @@ registerPlugin({
             let cmd;
             try {
                 cmd = command.createCommand(name);
+                engine.log(`Added command: ${name}`);
             } catch(ex) {
                 engine.log(`Skipping ${i+1}. command due to error: ${ex}`);
                 return;
@@ -214,12 +300,30 @@ registerPlugin({
 
             if (cfg.restrict) {
                 cmd.checkPermission(client => {
-                    //TODO: check servergroup as configured
+                    const BLACKLIST = 0;
+                    let sglist = cfg.servergroups_list;
+                    let isBlacklist = (cfg.servergroups_type == BLACKLIST);
+
+                    if (!sglist || sglist.length == 0) {
+                        // returns true if (blacklist and empty) or false if (whitelist and empty)
+                        return isBlacklist
+                    }
+
+                    // returns true if (blacklist and not included) or (whitelist and included)
+                    return isBlacklist != client.getServerGroups().some(servergroup => 
+                        sglist.some(sgid => 
+                            servergroup.id() == sgid
+                        )
+                    )
                 })
 
                 if (cfg.cooldown) {
-                    //TODO: add cooldown as configured
-                    //cmd.addThrottle(...)
+                    const throttle = command.createThrottle()
+                    .initialPoints(1)
+                    .restorePerTick(1)
+                    .tickRate(cfg.cooldown * 1000); //ms
+
+                    cmd.addThrottle(throttle);
                 }
             }
 
@@ -233,7 +337,7 @@ registerPlugin({
                     switch (action.action_type) {
                         case 0:
                         case '0': // Respond with a message
-                            let text = action.response_test;
+                            let text = action.response_text;
                             if (!text || text.length < 1) {
                                 engine.log(`Warning in ${i+1}. command: No message text set in ${j+1}. action.`);
                                 return;
@@ -266,7 +370,7 @@ registerPlugin({
                     }
                 });
                 cmd.exec((client, args, reply, ev) => {
-                    if (cfg.restrict) {
+                    /*if (cfg.restrict) {
                         if (ev.mode == 3 && cfg.disable_serverchat) {
                             engine.log('Ignoring command since serverchat is disabled.');
                             return;
@@ -279,7 +383,7 @@ registerPlugin({
                             engine.log('Ignoring command since privatechat is disabled.');
                             return;
                         }
-                    }
+                    }*/
 
                     /** @implements {Context} */
                     let ctx = {
@@ -287,11 +391,16 @@ registerPlugin({
                         args: args,
                         reply: reply,
                         ev: ev,
+                        channel: ev.channel,
                         message: ev.message,
                     }
 
                     funcs.forEach(func => {
-                        func(ctx);
+                        try {
+                            func(ctx);
+                        } catch(ex) {
+                            engine.log(ex);
+                        }
                     });
                 })
             }
@@ -300,69 +409,58 @@ registerPlugin({
 
     /**
      * Formats a string with placeholders.
-     * @param {string} str
-     * @param {Context} ctx
-     * @param {object} placeholders
+     * @param {string} str Format String
+     * @param {Context} ctx Context
+     * @param {object} placeholders Placeholders
      */
     function format(str, ctx, placeholders) {
-        //TODO: implement formatter
-        return str;
+        return str.replace(/((?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{([0-9_\-a-zA-Z]+)(?:\((.+)\))?\})/g, (m, str, placeholder, argsStr) => {
+            if (str) {
+                return str.replace(/(?:{{)|(?:}})/g, m => m[0]);
+            } else {
+                if (!placeholders.hasOwnProperty(placeholder) || typeof placeholders[placeholder] !== 'function') {
+                    engine.log(`Unknown placeholder: ${placeholder}`);
+                    return `{${placeholder}: unknown placeholder}`;
+                }
+                let args = [];
+                if (argsStr && argsStr.length > 0) {
+                    args = argsStr.split(/\s*(?<!\\)(?:,|$)\s*/);
+                    args.map(arg => arg.replace('\\,', ','));
+                }
+
+                let result = `{${placeholder}: empty}`;
+                try {
+                    result = placeholders[placeholder](ctx, ...args);
+                } catch(ex) {
+                    result = `{${placeholder}: error}`;
+                    engine.log(`placeholder "${placeholder}" caused an error: ${ex}`);
+                }
+
+                return result;
+            }
+        });
+    }
+    
+    /**
+     * @returns a random integer between two values, inclusive
+     * @param {number} min 
+     * @param {number} max
+     */
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     /**
-     * Loggs a message, requires engine
-     * 
-     * @param {string} str Message
-     * @param {Client} client Client
-     * @return {string}
+     * Returns a formatted string from a track.
+     *
+     * @param {Track} track
+     * @returns {string} formatted string
      */
-    function replacePlaceholders(str, client) {
-        var clients = backend.getClients()
-        var channels = backend.getChannels()
-        var discord = engine.getBackend() === 'discord'
-
-        str = str.replace(/%(username|user|name|nick)%/gi, client.name())
-        .replace(/%uid%/gi, client.uid())
-        .replace(/%dbid%/gi, discord ? client.uid().split('/')[1] : client.databaseID())
-        .replace(/%description%/gi, client.description())
-        .replace(/%ping%/gi, client.getPing().toString())
-        .replace(/%total_connections%/gi, client.getTotalConnections().toString())
-        .replace(/%packetloss%/gi, client.getPacketLoss().toString())
-        .replace(/%bytes_sent%/gi, client.getBytesSent().toString())
-        .replace(/%bytes_received%/gi, client.getBytesReceived().toString())
-        .replace(/%ip%/gi, client.getIPAddress())
-        .replace(/%first_join%/gi, client.getCreationTime() > 0 ? new Date(client.getCreationTime()).toLocaleDateString() : 'unknown')
-        .replace(/%os%/gi, client.getPlatform())
-        .replace(/%version%/gi, client.getVersion())
-        .replace(/%mention%/gi, discord ? '<@'+client.uid().split('/')[1]+'>' : client.getURL())
-        .replace(/%clients_count%/gi, clients.length.toString())
-        .replace(/%clients%/gi, clients.reduce(function (clstr, client) {
-            return clstr += (clstr ? ', ' : '') + client.name()
-        }, ''))
-        .replace(/%channels_count%/gi, channels.length.toString())
-
-        return str
-    }
-
-    /**
-     * Checks if a client is blacklisted
-     * 
-     * @param {Client} client
-     * @return {boolean}
-     */
-    function isBlacklisted(client) {
-        var isBlacklist = (config.blackOrWhite == 0)
-
-        if (!config.sgList || config.sgList.length == 0) {
-            // returns false if (blacklist and empty) or true if (whitelist and empty)
-            return !isBlacklist
-        }
-
-        // returns true if (blacklist and included) or (whitelist and not included)
-        return isBlacklist == client.getServerGroups().some(function (servergroup) {
-            return config.sgList.some(function (item) {
-                return servergroup.id() == item.servergroup
-            })
-        })
+    function formatTrack(track) {
+        let title = track.tempTitle() || track.title();
+        let artist = track.tempArtist() || track.artist();
+        return artist ? `${artist} - ${title}` : title;
     }
 })
